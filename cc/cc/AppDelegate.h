d7d26a29a77337410dec537b8b9367bf064366cd
licong <queen@QueendeiMac.local>
2016-07-12 10:46:12 +0800

    adsa

---
+++ cc/cc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cc
+//
+//  Created by Queen on 16/7/12.
+//  Copyright © 2016年 WFX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cc/cc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cc
+//
+//  Created by Queen on 16/7/12.
+//  Copyright © 2016年 WFX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
